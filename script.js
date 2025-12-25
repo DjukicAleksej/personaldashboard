1d28ae741f747ce360186ce5d3f5fe3f3087f6b7
DjukicAleksej <aleksejdjukicets@gmail.com>
2025-12-25 12:56:11 +0100

    Added the html for theoptions to edit or edelee the todos

--- script.js
+++ script.js
@@ -68,7 +68,7 @@ todoInput.addEventListener('keypress', e => {
 loadTodos();
 
 // -- COUNDOWN CARD ---
-const countdownEl = document.getElementById('countdown');
+const countdownEl = document.getElementById('Countdown');
 const targetDate = new Date("2026-01-01T00:00:00");
 
 function updateCountdown() {
