f20f1a4f77c808c635dc7074506ae1aa773b9972
DjukicAleksej <aleksejdjukicets@gmail.com>
2025-12-25 12:43:10 +0100

    Added countdown card and function that will display days hours minutes second

--- script.js
+++ script.js
@@ -67,6 +67,23 @@ todoInput.addEventListener('keypress', e => {
 });
 loadTodos();
 
+// -- COUNDOWN CARD ---
+const countdownEl = document.getElementById('countdown');
+const targetDate = new Date("2026-01-01T00:00:00");
+
+function updateCountdown() {
+    const now = new Date();
+    const diff = targetDate - now;
+    const days = Math.floor(diff / (1000*60 * 60 * 24));
+    const hours = Math.floor((diff/ (1000 * 60 * 60)) % 24);
+    const minutes = Math.floor((diff/ (1000*60)) % 60);
+    const seconds = Math.floor((diff / 1000) % 60);
+
+    countdownEl.textContent = `${days}d ${hours}h ${minutes}m ${seconds}s`;
+}
+setInterval(updateCountdown,1000);
+updateCountdown();
+
 
 
 
