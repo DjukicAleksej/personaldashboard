b9e2aa75f11fccd1b80e3b83d13f5ef4ae498421
DjukicAleksej <aleksejdjukicets@gmail.com>
2025-12-27 00:23:50 +0100

    Format time helper function is over! I return minutes and seconds formated in a string!

--- script.js
+++ script.js
@@ -85,6 +85,10 @@ function updateTimer(){
     }
 }
 
+function formatTime(minutes,seconds){
+    return
+    `${String(minutes).padStart(2,'0')}:${String(seconds).padStart(2,'0')}`;
+}
 
 
 function loadTodos() {
